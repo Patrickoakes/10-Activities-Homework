f4401d8af71de1b6ece4ac0b9239835680ae5e59
Patrickoakes <56033218+Patrickoakes@users.noreply.github.com>
2019-12-12 07:30:04 -0800

    add node

---
+++ modern-node/05 asynchonous-vs-synch/index.js
@@ -0,0 +1,37 @@
+const http = require('http')
+const fs = require('fs')
+
+// NON BLOCKING
+
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const homePage = fs.readFileSync('index.html')
+
+
+const server = http.createServer((request, response) => {
+    if (request.url === '/about') {
+        return response.end(aboutPage)
+    } else if (request.url === '/contact') {
+        return response.end(contactPage)
+    } else if (request.url === '/') {
+        return response.end(homePage)
+    } else {
+        response.writeHead(404)
+
+        response.end('THE PAGE WAS NOT FOUND.')
+    }
+})
+
+server.listen(3000)
+© 2019 GitHub, Inc.
+    Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
---
+++ modern-node/07-intro to express/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (request, response) => {
+    response.json({
+        name: 'kati frantz'
+    })
+})
+
+app.get('/about', (request, response) => {
+    response.send({
+        name: 'kati frantz'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000')
+})
+© 2019 GitHub, Inc.
