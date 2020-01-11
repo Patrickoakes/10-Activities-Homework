40cfbb0f1dc8eee614ee366ebfd03731cc9246f2
Patrick Oakes <patrickoakes1972@gmail.com>
2020-01-11 09:11:16 -0800

    comm

--- 01-Activities/14-AppendFileAddToExistingFileVarPics/appendFile.js
+++ 01-Activities/14-AppendFileAddToExistingFileVarPics/appendFile.js
@@ -2,11 +2,16 @@
 var fs = require("fs");
 
 // Next, we store the text given to us from the command line.
-var text = process.argv[2];
+const [node,file,...args] = process.argv;
 
+console.log(node);
+console.log(file);
+console.log(node);
+const text = args.join(' ');
 // Next, we append the text into the "sample.txt" file.
 // If the file didn't exist, then it gets created on the fly.
-fs.appendFile("sample.txt", text, function(err) {
+//var text = process.argv;
+fs.appendFile("sample.txt", text + '/n', function(err) {
 
   // If an error was experienced we will log it.
   if (err) {
